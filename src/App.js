a828b6a5b885cfb86e7ab90e8385352a644342e8
nobody00O <vajpayee011@gmail.com>
2026-08-29 19:05:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          herf//www.robotstxt.org/robotstxt.html
+          herf="www.reactapplication.com";
           target="_blank"
           rel="noopener noreferrer"
         >
