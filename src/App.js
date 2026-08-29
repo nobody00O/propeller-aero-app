b1e9e3e2ecda4d24963bec8d056726cfd4aa92ee
nobody00O <vajpayee011@gmail.com>
2026-08-29 19:05:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          herf="www.reactapplication.com";
+          herf ="www.reactapplication.com";
           target="_blank"
           rel="noopener noreferrer"
         >
